d0e8f6357065138ef8d7acd720626159572d5ef1
Alan Steremberg <alan.steremberg@gmail.com>
2021-01-16 14:12:16 -0800

    changes

--- Arcade-Druaga.sv
+++ Arcade-Druaga.sv
@@ -112,57 +112,16 @@ assign VIDEO_ARX = (!ar) ? ((status[2] ) ? 8'd4 : 8'd3) : (ar - 1'd1);
 assign VIDEO_ARY = (!ar) ? ((status[2] ) ? 8'd3 : 8'd4) : 12'd0;
 
 
-`include "build_id.v"
-localparam CONF_STR = {
-	"A.DKONGJ;;",
-	"-;",
-	"H0O2,Orientation,Vert,Horz;",
 
 `include "build_id.v" 
 
 localparam CONF_STR = {
 	"A.Druaga;;",
-	"H0-;",
 	"H0OJK,Aspect ratio,Original,Full Screen,[ARC1],[ARC2];",
-	"HFO1,Aspect Ratio,Original,Wide;",
-	"HFO2,Orientation,Vert,Horz;",
+	"H0O2,Orientation,Vert,Horz;",
 	"O35,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%,CRT 75%;",
 	"-;",
-	"H1T7,:: Druaga DipSW Setting :;",
-	"H2T7,:: Mappy DipSW Setting :;",
-	"H3T7,:: DigDug2 DipSW Setting :;",
-	"H4T7,:: Motos DipSW Setting :;",
-	"-;",
-
-	"H1O89,Lives,3,2,1,5;",
-
-	"H2OAC,Rank,A,B,C,D,E,F,G,H;",
-	"H2OHI,Lives,3,5,1,2;",
-	"H2OEG,Extend,M1,M2,M3,M4,M5,M6,M7,None;",
-	"H2OD,Demo Sound,On,Off;",
-	"H2O6,Round Progress,Off,On;",
-	
-	"H3OJ,Lives,3,5;",
-	"H3OKL,Extend,30k/80k,30k/100k,30k/120k,30k/150k;",
-	"H3OM,Level Select,Off,On;",
-	
-	"H4OO,Rank,A,B;",
-	"H4ON,Lives,3,5;",
-	"H4OPQ,Extend,10k/30k/ev.50k,20k/ev.50k,30k/ev.70k,20k/70k;",
-	"H4OR,Demo Sound,On,Off;",
-
-	"-;",
-/*	"H1OV,Cabinet,Upright,Cocktail;",
-	"H2OV,Cabinet,Upright,Cocktail;",
-	"H3OV,Cabinet,Upright,Cocktail;",
-	"H4OV,Cabinet,Upright,Cocktail;", */
-	"H1OU,Service Mode,Off,On;",
-	"H2OU,Service Mode,Off,On;",
-	"H3OU,Service Mode,Off,On;",
-	"H4OU,Service Mode,Off,On;",
-	"H1OT,Freeze,Off,On;",
-	"H2OT,Freeze,Off,On;",
-	"H3OT,Freeze,Off,On;",
+	"DIP;",
 	"-;",
 	"R0,Reset;",
 	"J1,Trig1,Trig2,Start 1P,Start 2P,Coin;",
@@ -176,65 +135,28 @@ localparam CONF_STR = {
 // RAOfffmxttmmmmmmmmmddddooooo FSC
 
 // (common)
-wire		  dcFreeze   = status[29];
-wire		  dcService  = status[30];
-//wire	  dcCabinet  = status[31];
 wire	 	  dcCabinet  = 1'b0;				// (upright only)
 
 
-// The Tower of Druaga [t]
-wire [1:0] dtLives	 = status[9:8];
+reg mod_druaga = 0;
+reg mod_digdug = 0;
+reg mod_mappy  = 0;
+reg mod_motos  = 0;
 
-wire [7:0] tDSW0 = {2'd0,dtLives,4'd0};
-wire [7:0] tDSW1 = {dcCabinet,6'd0,dcFreeze};
-wire [7:0] tDSW2 = {tDSW1[3:0],dcService,3'd0};
+always @(posedge clk_sys) begin
+	reg [7:0] mod = 0;
+	if (ioctl_wr & (ioctl_index==1)) mod <= ioctl_dout;
+	
+	mod_druaga <= (mod == 1);
+	mod_mappy <= (mod == 2);
+	mod_digdug <= (mod == 3);
+	mod_motos <= (mod == 4);
+	
+end
 
-
-// Mappy [m]
-wire		  dmRoundP   = status[6];
-wire [2:0] dmRank		 = status[12:10];
-wire 		  dmDemoSnd	 = status[13];
-wire [2:0] dmExtend	 = status[16:14];
-wire [1:0] dmLives    = status[18:17];
-
-wire [7:0] mDSW0 = {dcFreeze,dmRoundP,dmDemoSnd,2'd0,dmRank};
-wire [7:0] mDSW1 = {dmLives,dmExtend,3'd0};
-wire [7:0] mDSW2 = {{2{dcService,dcCabinet,2'd0}}};
-
-
-// DigDug2 [d]
-wire		  ddLives    = status[19];
-wire [1:0] ddExtend   = status[21:20];
-wire 		  ddLevelSel = status[22];
-
-wire [7:0] dDSW0 = {2'd0,ddLives,5'd0};
-wire [7:0] dDSW1 = {dcCabinet,3'd0,dcFreeze,ddLevelSel,ddExtend};
-wire [7:0] dDSW2 = {dDSW1[3:0],dcService,3'd0};
-
-
-
-// Motos [o]
-wire       doLives    = status[23];
-wire       doRank     = status[24];
-wire [1:0] doExtend   = status[26:25];
-wire 		  doDemoSnd  = status[27];
-
-wire [7:0] oDSW0 = {doDemoSnd,doExtend,doRank,doLives,3'd0};
-wire [7:0] oDSW1 = {dcService,dcCabinet,6'd0};
-wire [7:0] oDSW2 = {8'd0};
-
-
-reg   [3:0] tno  = 0;
-
-// Title specific DipSWs
-//
-//	DIPSW[23:20] = IsMappy ? DIPSW[19:16] : DIPSW[11:8];
-//
-wire [23:0] DSWs = (tno==1) ? {tDSW2,tDSW1,tDSW0} :
-						 (tno==2) ? {mDSW2,mDSW1,mDSW0} :
-						 (tno==3) ? {dDSW2,dDSW1,dDSW0} :
-						 (tno==4) ? {oDSW2,oDSW1,oDSW0} : 0;
-
+// DIP SWITCHES
+reg [7:0] sw[8];
+always @(posedge clk_sys) if (ioctl_wr && (ioctl_index==254) && !ioctl_addr[24:3]) sw[ioctl_addr[2:0]] <= ioctl_dout;
 
 
 ////////////////////   CLOCKS   ///////////////////
@@ -266,7 +188,6 @@ wire  [7:0] ioctl_index;
 wire [10:0] ps2_key;
 wire [15:0] joystk1, joystk2;
 
-wire [14:0] menumask = ~(15'd1 << tno);
 wire [21:0] gamma_bus;
 
 hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
@@ -279,7 +200,7 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 	.buttons(buttons),
 
 	.status(status),
-	.status_menumask({direct_video,menumask}),
+	.status_menumask({direct_video}),
 
 	.forced_scandoubler(forced_scandoubler),
 	.gamma_bus(gamma_bus),
@@ -296,85 +217,30 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 	.ps2_key(ps2_key)
 );
 
-// Retleave Title No.
-always @(posedge clk_sys) begin
-	if (ioctl_wr & (ioctl_index==1)) tno <= ioctl_dout[3:0];
-end
 
-wire       pressed = ps2_key[9];
-wire [8:0] code    = ps2_key[8:0];
-always @(posedge clk_sys) begin
-	reg old_state;
-	old_state <= ps2_key[10];
-	
-	if(old_state != ps2_key[10]) begin
-		casex(code)
-			'hX75: btn_up          <= pressed; // up
-			'hX72: btn_down        <= pressed; // down
-			'hX6B: btn_left        <= pressed; // left
-			'hX74: btn_right       <= pressed; // right
-			'h029: btn_trig1       <= pressed; // space
-			'h014: btn_trig2       <= pressed; // ctrl
-			'h005: btn_one_player  <= pressed; // F1
-			'h006: btn_two_players <= pressed; // F2
-
-			// JPAC/IPAC/MAME Style Codes
-			'h016: btn_start_1     <= pressed; // 1
-			'h01E: btn_start_2     <= pressed; // 2
-			'h02E: btn_coin_1      <= pressed; // 5
-			'h036: btn_coin_2      <= pressed; // 6
-			'h02D: btn_up_2        <= pressed; // R
-			'h02B: btn_down_2      <= pressed; // F
-			'h023: btn_left_2      <= pressed; // D
-			'h034: btn_right_2     <= pressed; // G
-			'h01C: btn_trig1_2     <= pressed; // A
-			'h01B: btn_trig2_2     <= pressed; // S
-		endcase
-	end
-end
-
-reg btn_up    = 0;
-reg btn_down  = 0;
-reg btn_right = 0;
-reg btn_left  = 0;
-reg btn_trig1 = 0;
-reg btn_trig2 = 0;
-reg btn_one_player  = 0;
-reg btn_two_players = 0;
-
-reg btn_start_1 = 0;
-reg btn_start_2 = 0;
-reg btn_coin_1  = 0;
-reg btn_coin_2  = 0;
-reg btn_up_2    = 0;
-reg btn_down_2  = 0;
-reg btn_left_2  = 0;
-reg btn_right_2 = 0;
-reg btn_trig1_2  = 0;
-reg btn_trig2_2  = 0;
 
 
 wire bCabinet  = dcCabinet;
 
-wire m_up2     = btn_up_2    | joystk2[3];
-wire m_down2   = btn_down_2  | joystk2[2];
-wire m_left2   = btn_left_2  | joystk2[1];
-wire m_right2  = btn_right_2 | joystk2[0];
-wire m_trig21  = btn_trig1_2 | joystk2[4];
-wire m_trig22  = btn_trig2_2 | joystk2[5];
+wire m_up2     = joystk2[3];
+wire m_down2   = joystk2[2];
+wire m_left2   = joystk2[1];
+wire m_right2  = joystk2[0];
+wire m_trig21  = joystk2[4];
+wire m_trig22  = joystk2[5];
 
-wire m_start1  = btn_one_player  | joystk1[6] | joystk2[6] | btn_start_1;
-wire m_start2  = btn_two_players | joystk1[7] | joystk2[7] | btn_start_2;
+wire m_start1  = joystk1[6] | joystk2[6];
+wire m_start2  = joystk1[7] | joystk2[7];
 
-wire m_up1     = btn_up      | joystk1[3] | (bCabinet ? 1'b0 : m_up2);
-wire m_down1   = btn_down    | joystk1[2] | (bCabinet ? 1'b0 : m_down2);
-wire m_left1   = btn_left    | joystk1[1] | (bCabinet ? 1'b0 : m_left2);
-wire m_right1  = btn_right   | joystk1[0] | (bCabinet ? 1'b0 : m_right2);
-wire m_trig11  = btn_trig1   | joystk1[4] | (bCabinet ? 1'b0 : m_trig21);
-wire m_trig12  = btn_trig2   | joystk1[5] | (bCabinet ? 1'b0 : m_trig22);
+wire m_up1     = joystk1[3] | (bCabinet ? 1'b0 : m_up2);
+wire m_down1   = joystk1[2] | (bCabinet ? 1'b0 : m_down2);
+wire m_left1   = joystk1[1] | (bCabinet ? 1'b0 : m_left2);
+wire m_right1  = joystk1[0] | (bCabinet ? 1'b0 : m_right2);
+wire m_trig11  = joystk1[4] | (bCabinet ? 1'b0 : m_trig21);
+wire m_trig12  = joystk1[5] | (bCabinet ? 1'b0 : m_trig22);
 
-wire m_coin1   = btn_one_player | btn_coin_1 | joystk1[8];
-wire m_coin2   = btn_two_players| btn_coin_2 | joystk2[8];
+wire m_coin1   = joystk1[8];
+wire m_coin2   = joystk2[8];
 
 
 ///////////////////////////////////////////////////
@@ -391,9 +257,11 @@ always @(posedge clk_hdmi) begin
 	ce_pix  <= old_clk & ~ce_vid;
 end
 
-wire no_rotate = status[2] & ~direct_video;
+wire no_rotate = status[2] | direct_video;
+wire rotate_ccw = 0;
+screen_rotate screen_rotate (.*);
 
-arcade_rotate_fx #(288,224,12,0) arcade_video
+arcade_video #(288,12) arcade_video
 (
 	.*,
 
@@ -437,12 +305,17 @@ wire  [7:0] oPIX;
 wire  [7:0] oSND;
 
 fpga_druaga GameCore ( 
-	.RESET(iRST),.MCLK(clk_48M),
+	.RESET(iRST),
+	.MCLK(clk_48M),
 	.PH(HPOS),.PV(VPOS),.PCLK(PCLK),.POUT(oPIX),
 	.SOUT(oSND),
 
-	.INP0(INP0),.INP1(INP1),.INP2(INP2),
-	.DSW0(DSWs[7:0]),.DSW1(DSWs[15:8]),.DSW2(DSWs[23:16]),
+	.INP0(INP0),
+	.INP1(INP1),
+	.INP2(INP2),
+	.DSW0(sw[0]),
+	.DSW1(sw[1]),
+	.DSW2(sw[2]),
 	
 	.ROMCL(clk_sys),.ROMAD(ioctl_addr),.ROMDT(ioctl_dout),.ROMEN(ioctl_wr & (ioctl_index == 0))
 );
